52cf4f7514e3d77bb2a1ac5aeca46fb6c72dc3ce
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-02-02 18:58:24 -0500

    ya funcionan create y update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :pending_order
+
+  def pending_order
+    if current_user
+      pending_order = current_user.orders.where(state: "pending").take || nil
+      pending_order = current_user.orders.create(state: "pending") unless pending_order.present?
+      return pending_order
+    end
+  end
+
+  protected
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,22 +21,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name photo])
   end
 
-  # def set_current_order
-  #   if session[:user_id]
-  #     cookies[:order_id] = "test"
-  #   end
-  #   cookies[:order_id] =  ""
-  # end
-
-  protected
-
-   def after_sign_in_path_for(resource)
-     session[:current_order] = "probando desde devise"
-    #byebug
+  def after_sign_in_path_for(resource)
+    # pending_order = current_user.orders.where(state: "pending").first
+    # session[:current_order] = pending_order || Order.create(state: "pending") if user_signed_in?
+    # stored_location_for(resource) || root_path
     stored_location_for(resource) || root_path
-   end
-
-  # def set_order
-  #   cookies[:current_order] = "true"
-  # end
+  end
 end
