44e4590e368554da929ed97f139a11f4e42070fa
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-02-03 17:46:52 -0500

    tabla photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_190156) do
+ActiveRecord::Schema.define(version: 2022_02_03_224449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2022_02_03_190156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "print_id"
+    t.string "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["print_id"], name: "index_photos_on_print_id"
   end
 
   create_table "prints", force: :cascade do |t|
