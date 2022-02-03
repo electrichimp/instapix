d13bb634ec5e1fd57470525bb2a6eb9fe7351a10
luisaugusto <luis.augusto.u@gmail.com>
2022-02-03 11:05:05 -0500

    cambios modelo order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,5 @@
 class Order < ApplicationRecord
-  enum status: %i[pending paid printing ready_to_deliver delivered cancelled]
+  enum status: %i[editing pending paid printing ready_to_deliver delivered cancelled]
   belongs_to :user
-<<<<<<< HEAD
-=======
-  enum state: %i[pending paid printing ready_to_deliver delivered cancelled]
->>>>>>> dcdfac02e5c9cbdcc6f4ac4f13f29b1807b1ef17
   has_many :prints
 end
